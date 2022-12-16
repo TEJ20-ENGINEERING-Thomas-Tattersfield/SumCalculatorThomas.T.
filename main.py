e49698f763ec3fd2dde3bc29c6971bdebe63ddc7
Thomas Tattersfield <118993239+Thomas-Tatt@users.noreply.github.com>
2022-12-16 17:17:02 +0000

    changed some of the later programming

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # asks user for input
 User_number = int(input("Please input a positive interger: "))
-# initializes I to 0
+# initializes I to 0 and shows how the new number is calculated
 i = 0
+New_number = i + 1 
 # shows the formula and Calculations
-while i <= User_number:
-  print ("{} * {} = {}". format (i, User_number, i * User_number))
+while i < User_number:
+  print ("{} + {} = {}". format (i, New_number, i + New_number))
   i += 1 
